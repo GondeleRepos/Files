ccac7690d2ef93de565638f4cdcf0df3eb070679
Daniel <121541904+GondeleRepos@users.noreply.github.com>
2024-05-01 19:18:31 +0200

    changed ElementHider to remove instead of hide

--- scripts/ElementRemover.js
+++ scripts/ElementRemover.js
@@ -1,10 +1,13 @@
-function hideElements() {
-    document.getElementById('footer').style.display = 'none';
-    var classesToHide = ['shoutbox', 'catalogNavNoFix right', 'catalogNavNoFix', 'carousel animeNews seriesNewsList'];
-    classesToHide.forEach(cls => {
-        document.querySelectorAll('.' + cls).forEach(el => el.style.display = 'none');
+function removeElements() {
+    var footer = document.getElementById('footer');
+    if (footer) footer.remove();
+
+    var classesToRemove = ['shoutbox', 'catalogNavNoFix right', 'catalogNavNoFix', 'carousel animeNews seriesNewsList'];
+    classesToRemove.forEach(cls => {
+        document.querySelectorAll('.' + cls).forEach(el => el.remove());
     });
-    var selectorsToHide = [
+
+    var selectorsToRemove = [
         'div#wrapper div.container div',
         'div.row:nth-child(8)',
         '.container > div:nth-child(13)',
@@ -13,10 +16,10 @@ function hideElements() {
         '.primary-navigation > ul:nth-child(1) > li:nth-child(4) > ul:nth-child(2) > li:nth-child(5)',
         '.primary-navigation > ul:nth-child(1) > li:nth-child(4) > ul:nth-child(2) > li:nth-child(6)'
     ];
-    selectorsToHide.forEach(sel => {
+    selectorsToRemove.forEach(sel => {
         var element = document.querySelector(sel);
         if (element) {
-            element.style.display = 'none';
+            element.remove();
         }
     });
 }
